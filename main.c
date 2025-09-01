5f89f28a23569df96176d7b996c389975e994d5a
uriy fedosov <fedosovyurij2017@gmail.com>
2025-09-01 17:29:05 +0300

    Settings vsc on statLinux 01/09/25 17:28

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	printf("Hello, World!\n");
+   printf("Hello, World!\n");
    getchar();
-	return 0;
+   return 0;
 }
--- test.c
+++ test.c
@@ -4,24 +4,24 @@ int i = 1;
 
 int main(void)
 {
-	int integer1;
-	int integer2;
+   int integer1;
+   int integer2;
    int integer3;
-	int summa;
+   int summa;
 
-	printf("Enter the first number\n");
-	scanf("%d", &integer1);
+   printf("Enter the first number\n");
+   scanf("%d", &integer1);
 
-	printf("Enter the second number\n");
-	scanf("%d", &integer2);
+   printf("Enter the second number\n");
+   scanf("%d", &integer2);
 
    printf("Enter the third number\n");
-	scanf("%d", &integer3);
+   scanf("%d", &integer3);
 
-	summa = integer1 + integer2 + integer3;
+   summa = integer1 + integer2 + integer3;
 
-	printf("Sum of numbers \n%d", summa);
-	getchar();
-	getchar();
-	return EXIT_SUCCESS;
+   printf("Sum of numbers \n%d", summa);
+   getchar();
+   getchar();
+   return EXIT_SUCCESS;
 }
